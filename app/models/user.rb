565e1164232500c98a928f66a8c5d74538035744
marcinkornek <marcinkornek@gmail.com>
2015-06-19 10:05:27 +0200

    [#3] Fix oauth login on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,6 @@ class User < ActiveRecord::Base
                         password_confirmation:password,
                       )
         user.send_activation_email = false
-        user.activate!
         user.save
         user.activate!
         return user
